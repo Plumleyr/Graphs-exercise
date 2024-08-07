7db17d4743c4c645170976f9da3247d6cf18d07b
Plumleyr <jrplumley@gmail.com>
2024-08-07 12:18:03 -0400

    added

---
+++ graph.js
@@ -0,0 +1,83 @@
+class Node {
+  constructor(value, adjacent = new Set()) {
+    this.value = value;
+    this.adjacent = adjacent;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.nodes = new Set();
+  }
+
+  // this function accepts a Node instance and adds it to the nodes property on the graph
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
+
+  // this function accepts an array of Node instances and adds them to the nodes property on the graph
+  addVertices(vertexArray) {
+    for (let v of vertexArray) {
+      this.nodes.add(v);
+    }
+  }
+
+  // this function accepts two vertices and updates their adjacent values to include the other vertex
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
+
+  // this function accepts two vertices and updates their adjacent values to remove the other vertex
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
+
+  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+  removeVertex(vertex) {
+    for (let v of vertex.adjacent) {
+      this.removeEdge(v, vertex);
+    }
+    this.nodes.delete(vertex);
+  }
+
+  // this function returns an array of Node values using DFS
+  depthFirstSearch(
+    start,
+    visited = new Set([start.value]),
+    nodeValArr = [start.value]
+  ) {
+    for (let v of start.adjacent) {
+      if (!visited.has(v.value)) {
+        nodeValArr.push(v.value);
+        visited.add(v.value);
+        this.depthFirstSearch(v, visited, nodeValArr);
+      }
+    }
+    return nodeValArr;
+  }
+
+  // this function returns an array of Node values using BFS
+  breadthFirstSearch(start) {
+    let queue = [start];
+    let visited = new Set([start.value]);
+    let nodeValArr = [];
+
+    while (queue.length > 0) {
+      let current = queue.shift();
+      nodeValArr.push(current.value);
+
+      for (let v of current.adjacent) {
+        if (!visited.has(v.value)) {
+          queue.push(v);
+          visited.add(v.value);
+        }
+      }
+    }
+
+    return nodeValArr;
+  }
+}
+
+module.exports = { Graph, Node };
